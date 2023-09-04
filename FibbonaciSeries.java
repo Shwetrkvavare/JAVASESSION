79db3eac054561afdd6c6ba374850381291c7181
Shwetrkvavare <143393876+Shwetrkvavare@users.noreply.github.com>
2023-09-04 14:53:37 +0530

    Add files via upload

---
+++ FibbonaciSeries.java
@@ -0,0 +1,21 @@
+package demo.java.datatype;
+
+public class FibbonaciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=0, n2=1, n3, count = 10,i;
+		System.out.println(n1 + " " + n2);
+		
+		for(i=2;i<count;i++)
+		{
+			n3 = n1 + n2;
+			System.out.println(" " + n3);
+			n1 = n2;
+			n2 = n3;
+			
+		}
+		
+	}
+
+}
