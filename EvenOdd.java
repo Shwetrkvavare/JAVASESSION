0882397862c137dea9eae72663131f7e5059a2ed
Shwetrkvavare <143393876+Shwetrkvavare@users.noreply.github.com>
2023-09-04 14:22:28 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+package demo.java.datatype;
+
+public class EvenOdd {
+	
+	int num = 22; 
+	
+	if(num % 2 == 0)
+	{
+		System.out.println(" Even Number");
+	}
+	else
+	{
+		System.out.println(" Odd Number");
+	}
+		
+}
