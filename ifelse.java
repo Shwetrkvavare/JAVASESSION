164eb810549ea3d065db68e21772f848728cf898
Shwetrkvavare <143393876+Shwetrkvavare@users.noreply.github.com>
2023-09-04 12:09:32 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,22 @@
+package demo.java.datatype;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int marks=65;
+		if(marks>80)
+		{
+			System.out.println("Distinction");
+		}
+		else if("marks>=60 && marks<=80")
+		{
+			System.out.println("First Class");
+		}
+		else
+		{
+			System.out.println("Pass");
+		}
+	}
+
+}
