5e0adbbf08933da1882d4fedd492c093223a4c97
Shwetrkvavare <143393876+Shwetrkvavare@users.noreply.github.com>
2023-09-04 12:11:17 +0530

    Add files via upload

---
+++ positiveornegativenumber.java
@@ -0,0 +1,22 @@
+package demo.java.datatype;
+
+public class positiveornegativenumber {
+
+	public static void main(String[] args) {
+		int number = -1;
+		if(number>0)
+		{
+			System.out.println("Number is Positive");
+		}
+		else if(number<0)
+		{
+			System.out.println("Number is Negative");
+		}
+		else
+		{
+			System.out.println("The Number is Zero");
+		}
+
+	}
+
+}
